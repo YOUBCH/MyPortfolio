c77b042abae7d3e5e1403ad39891256b2944c544
YOUBCH <youssefbachaoui02@gmail.com>
2022-09-17 21:24:22 +0200

    added media queries for responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import CustomButton from "../../custom/customButton/CustomButton";
 import "./Header.css";
 import resume from "../../myFiles/Youssef_Bachaoui_Resume.pdf";
-function Header(){
+function Header() {
 
     const [offset, setOffset] = useState(0);
 
     useEffect(() => {
         const navElements = document.querySelectorAll(".nav-element");
         let transitionDelay = 0;
-        for(let i = 0; i < navElements.length; i++){
+        for (let i = 0; i < navElements.length; i++) {
             navElements[i].style.transitionDelay = `${transitionDelay}ms`;
             navElements[i].style.top = "0px"
             transitionDelay += 100;
@@ -19,27 +19,43 @@ function Header(){
         window.removeEventListener('scroll', onScroll);
         window.addEventListener('scroll', onScroll, { passive: true });
         return () => window.removeEventListener('scroll', onScroll);
-        
+
     }, [])
 
     useEffect(() => {
         const element = document.querySelector('.header-container');
-        if(offset > 50) {
+        if (offset > 50) {
             element.style.height = "80px";
             element.style.background = "rgb(13 19 62 / 44%)";
             element.style.backdropFilter = "blur(10px)";
-        } else if(offset < 50){
+        } else if (offset < 50) {
             element.style.height = "150px";
             element.style.background = "var(--blue)";
             element.style.backdropFilter = "none";
         }
     }, [offset])
 
-    console.log(offset)
+    const changeMenuVisibility = () => {
+        const checkbox = document.querySelector('#check-menu');
+        const menu = document.querySelector('.navbar-container')
+        if (checkbox.checked) {
+            menu.style.transform = "translateX(0%)";
+        } else {
+            menu.style.transform = "translateX(100%)";
+        }
+    }
 
+    document.querySelectorAll(".nav-link").forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.stopImmediatePropagation();
+            const checkbox = document.querySelector('#check-menu');
+            checkbox.checked = false;
+            changeMenuVisibility();
+        }
+        )
+    });
 
-
-    return(
+    return (
         <div className="header-container">
             <div className="header">
                 <div className="logo-container">
@@ -50,8 +66,15 @@ function Header(){
                         <li className="nav-link nav-element"><a href="#about" >About</a></li>
                         <li className="nav-link nav-element">Experience</li>
                         <li className="nav-link nav-element">Contact</li>
-                        <li className="nav-btn nav-element"><a href={resume}  target="_blank"><CustomButton text={"Resume"} /></a></li>
+                        <li className="nav-btn nav-element"><a href={resume} target="_blank"><CustomButton text={"Resume"} /></a></li>
                     </ol>
+                </div>
+                <div className="hamburger-lines">
+                    <input className="checkbox" type="checkbox" id="check-menu" onChange={changeMenuVisibility} />
+
+                    <span className="line line1"></span>
+                    <span className="line line2"></span>
+                    <span className="line line3"></span>
                 </div>
             </div>
         </div>
